082dc81f1e6126f613438afb7edae6f88c676c5b
akaisuisei <b.watanabe@live.fr>
2012-12-16 14:55:42 +0100

    c est le scrolling et le background

---
+++ background/background/background/Background.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace background
+{
+    class Background
+    {
+        public Texture2D texture;
+        public Rectangle rectangle;
+
+        public void Draw(SpriteBatch spritebatch)
+        {
+            spritebatch.Draw(texture, rectangle, Color.White);
+        }
+    }
+}
---
+++ background/background/background/Scrolling.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace background
+{
+    class Scrolling : Background
+    {
+        private int speed = 3;
+
+        public Scrolling(Texture2D n_texture, Rectangle n_rectangle)
+        {
+            texture = n_texture;
+            rectangle = n_rectangle;
+        }
+
+        public void Update()
+        {
+            rectangle.Y -= speed;
+        }
+    }
+}
